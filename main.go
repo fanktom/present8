1e51d381d1163edf50734b9b7805132dd3d74e28
Thomas Fankhauser <tommy@system8.io>
2017-06-04 16:35:15 +0200

    Added initial compilation with slide navigation.

---
+++ main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/russross/blackfriday"
+)
+
+var (
+	title string
+)
+
+func main() {
+	// Flags
+	flag.StringVar(&title, "title", "present8", "title for the presentation")
+	flag.Parse()
+
+	// Read Input
+	if len(os.Args) < 2 {
+		panic("No input markdown file fiven")
+	}
+	input := os.Args[len(os.Args)-1]
+	markdown, err := ioutil.ReadFile(input)
+	if err != nil {
+		panic(err)
+	}
+
+	// Compile HTML
+	output := blackfriday.MarkdownCommon(markdown)
+
+	// Processing Pipeline
+	output = compileSlides(output)
+	output = surroundWithHTML(output)
+
+	// Write HTML
+	err = ioutil.WriteFile(input+".html", output, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func surroundWithHTML(html []byte) []byte {
+	out := fmt.Sprintf(`<!DOCTYPE html>
+<html>
+  <head>
+    <title>%v</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<style>
+			body, h1, h2, h3, h4, h5, p, ul, li {
+				margin: 0;
+				padding: 0;
+			}
+
+			section.slide {
+				min-height: 100vh;
+			}
+		</style>
+		<script>
+var p8 = {};
+function p8.allSlides() {
+  return document.querySelectorAll("section.slide");
+}
+
+function p8.currentSlide() {
+  var slides = p8.allSlides();
+  return slides[p8.currentSlideIndex()];
+}
+
+function p8.currentSlideIndex() {
+  var slides = p8.allSlides();
+  for(var i = slides.length-1; i >= 0; i--) {
+    if((window.pageYOffset +1) >= slides[i].offsetTop){
+      return i;
+    }
+  };
+  return 0;
+}
+
+function p8.p8.nextSlideIndex() {
+  var slides = p8.allSlides();
+  var current = p8.currentSlideIndex();
+  var next = current + 1;
+  if(next >= slides.length) {
+    return current;
+  }
+  return next;
+}
+
+function p8.p8.previousSlideIndex() {
+  var slides = p8.allSlides();
+  var current = p8.currentSlideIndex();
+  var prev = current - 1;
+  if(prev < 0) {
+    return 0;
+  }
+  return prev;
+}
+
+function p8.scrollToSlide(index) {
+  var slides = p8.allSlides();
+  var slide = slides[index];
+  window.scrollTo(0, slide.offsetTop);
+}
+
+function p8.nextSlide() {
+  p8.scrollToSlide(p8.p8.nextSlideIndex());
+}
+
+function p8.previousSlide() {
+  p8.scrollToSlide(p8.p8.previousSlideIndex());
+}
+
+function p8.registerKeyNavigation() {
+  document.onkeydown = function(e){
+    e.preventDefault();
+    // right, down and space
+    if(e.keyCode == 39 || e.keyCode == 40 || e.keyCode == 32) {
+      p8.nextSlide();
+    }
+    // left and up
+    if(e.keyCode == 37 || e.keyCode == 38) {
+      p8.previousSlide();
+    }
+  }
+}
+document.addEventListener('DOMContentLoaded', p8.registerKeyNavigation, false);
+		</script>
+  </head>
+  <body>
+		<section class='slide'>
+	`, title)
+	out += string(html)
+	out += `</section>
+	</body>
+</html>`
+	return []byte(out)
+}
+
+func compileSlides(html []byte) []byte {
+	out := string(html)
+	out = strings.Replace(out, "<hr />", "</section>\n<section class='slide'>", -1)
+	return []byte(out)
+}
